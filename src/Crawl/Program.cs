41db89699a6886ee420000599c7690e6b61702b4
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-06-07 23:20:59 +0100

    figure out how to download a page

--- src/Crawl/Program.cs
+++ src/Crawl/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Crawler;
+namespace Crawl;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var seed = "https://www.bbc.co.uk/news";
+
+        var crawler = new WebCrawler();
+        await crawler.Crawl(seed);
+    }
+}
---
+++ src/Crawler/WebCrawler.cs
@@ -0,0 +1,18 @@
+﻿namespace Crawler;
+
+public class WebCrawler
+{
+    public async Task Crawl(string seed, CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine($"Visiting {seed}");
+
+        var client = new HttpClient();
+        var uri = new Uri(seed);
+        var html = await client.GetStringAsync(uri, cancellationToken);
+
+        Console.WriteLine($"""
+                            Website: {seed}
+                            Content: {html}
+                           """);
+    }
+}
