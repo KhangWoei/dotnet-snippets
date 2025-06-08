a8ac9c726f4ac3e634ca7cc79d451193153ef39f
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-06-08 18:33:00 +0100

    setup basic command line

--- src/Crawl/Program.cs
+++ src/Crawl/Program.cs
@@ -1,13 +1,24 @@
 ﻿using Crawler;
+using System.CommandLine;
+
 namespace Crawl;
 
 public class Program
 {
-    public static async Task Main(string[] args)
+    public static int Main(string[] args)
     {
-        var seed = "https://www.bbc.co.uk/news";
+        var seed = new Option<string>(name: "--seed", description: "Website to start crawling from.");
 
-        var crawler = new WebCrawler();
-        await crawler.Crawl(seed);
+        var rootCommand = new RootCommand("Web crawler.");
+
+        rootCommand.AddOption(seed);
+
+        rootCommand.SetHandler(async (seed) =>
+        {
+            var crawler = new WebCrawler();
+            await crawler.Crawl(seed);
+        }, seed);
+
+        return rootCommand.Invoke(args);
     }
 }
