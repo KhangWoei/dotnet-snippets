5ed6e82b79d8e7f3780d517528ed00a8ffa4698e
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-06-11 00:06:39 +0100

    add some tests

---
+++ src/Crawler.Tests/RobotsParserTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Crawler.Tests;
+
+public class RobotsParserTests
+{
+    [Test]
+    public void DisallowListForAllBots_Parsed()
+    {
+        var robots = """
+                     User agent: *
+                     
+                     Disallow: test 
+                     """;
+
+        var expected = new string[] { "test" };   
+
+        var actual = RobotsParser.Parse(robots).ToArray();
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void Comments_Ignored()
+    {
+        var robots = """
+                     #Disallow: test
+                     """;
+
+        var expected = Array.Empty<string>();
+
+        var actual = RobotsParser.Parse(robots).ToArray();
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+}
