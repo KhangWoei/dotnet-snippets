f6e1a1f30c01fb0669ef2c5477a2804b7877ce5f
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-06-17 23:53:40 +0100

    use a singleton httpcleitn with fixed connection pool for now

--- src/Crawler/CrawlerDependencies.cs
+++ src/Crawler/CrawlerDependencies.cs
@@ -7,10 +7,18 @@ public static class CrawlerDependencies
 {
     public static void UserCrawler(this IServiceCollection services)
     {
-        services.AddTransient<WebCrawler>(provider => 
+        services.AddTransient<WebCrawler>(provider =>
                 new WebCrawler(provider.GetRequiredService<ILinkVisitor>()));
 
-        services.AddTransient<HttpClient>();
+        services.AddSingleton<HttpClient>(provider => new HttpClient(new SocketsHttpHandler
+        {
+            PooledConnectionLifetime = TimeSpan.FromMinutes(30),
+            PooledConnectionIdleTimeout = TimeSpan.FromSeconds(10)
+        })
+        {
+            DefaultRequestHeaders = { { "accept", "text/html" } },
+            Timeout = TimeSpan.FromSeconds(15)
+        });
         services.AddTransient<ILinkVisitor, LinkVisitor>();
     }
 }
