07444b9d7fec535d1661f4df8c09105c6bcabd27
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-06-18 23:19:55 +0100

    push queue logic into CrawlSource

--- src/Crawler/CrawlSource.cs
+++ src/Crawler/CrawlSource.cs
@@ -9,7 +9,9 @@ internal sealed class CrawlSource
         Base = uri;
         Depth = depth;
         Robot = robot;
-        Seen = [];
+        Seen = [uri];
+        Queue = new PriorityQueue<Uri, int>();
+        Queue.Enqueue(uri, -1);
     }
     
     public static async Task<CrawlSource> Create(string seed, int depth, CancellationToken cancellationToken)
@@ -20,14 +22,16 @@ internal sealed class CrawlSource
         return new CrawlSource(uri, depth, robots);
     }
     
-    public Uri Base { get; init; }
+    public Uri Base { get; }
     
-    public int Depth { get; init; }
+    public int Depth { get; }
     
-    public Robot Robot { get; init; }
+    public Robot Robot { get; }
     
     // TODO -  Kinda want to use a trie tree, would make it easier to reconstruct the output or map of the site it just crawled
-    public HashSet<Uri> Seen { get; init; }
+    public HashSet<Uri> Seen { get; }
+    
+    public PriorityQueue<Uri, int> Queue { get; }
     
     public bool CanVisit(Uri uri) => !Seen.Contains(uri) && !Robot.Disallowed.Contains(uri);
 }
--- src/Crawler/WebCrawler.cs
+++ src/Crawler/WebCrawler.cs
@@ -10,40 +10,27 @@ public class WebCrawler(ILinkVisitor linkVisitor)
     {
         var source = await CrawlSource.Create(seed, depth, cancellationToken);
 
-        var visitQueue = new Queue<Uri>();
-        visitQueue.Enqueue(source.Base);
-        
-        var currentDepth = 0;
-        while (visitQueue.Count > 0 && currentDepth < source.Depth)
+        while (source.Queue.TryDequeue(out var current, out var currentDepth) && currentDepth < source.Depth)
         {
-            var width = visitQueue.Count;
-
-            for (var i = 0; i < width; i++)
+            var html = await linkVisitor.VisitAsync(current, cancellationToken);
+            if (!string.IsNullOrEmpty(html))
             {
-                var current = visitQueue.Dequeue();
-                source.Seen.Add(current);
-
-                var html = await linkVisitor.VisitAsync(current, cancellationToken);
-                if (!string.IsNullOrEmpty(html))
+                foreach (var link in LinkHarvester.Harvest(html))
                 {
-                    foreach (var link in LinkHarvester.Harvest(html))
+                    // TODO - handle links that are not relative to the current source
+                    if (source.Base.IsBaseOf(link))
                     {
-                        // TODO - handle links that are not relative to the current source
-                        if (source.Base.IsBaseOf(link))
+                        if (source.CanVisit(link))
                         {
-                            if (source.CanVisit(link))
-                            {
-                                visitQueue.Enqueue(link);
-                            }
+                            source.Seen.Add(link);
+                            source.Queue.Enqueue(link, currentDepth + 1);
                         }
-                        // else send a new seed or base uri request and have the orchestration service handle it
                     }
+                    // else send a new seed or base uri request and have the orchestration service handle it
                 }
-
-                Thread.Sleep(source.Robot.DelayMs);
             }
-            
-            currentDepth++;
+
+            Thread.Sleep(source.Robot.DelayMs);
         }
     }
 }
