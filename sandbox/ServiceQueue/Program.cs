21fb93b0cfd653bd4c31fe1127a38ea4eb21e5e0
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-06-24 22:44:33 +0100

    add controller

--- sandbox/ServiceQueue/Program.cs
+++ sandbox/ServiceQueue/Program.cs
@@ -1,7 +1,13 @@
 using ServiceQueue;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<Controller>();
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<IServiceQueue>(_ => new ServiceQueue.ServiceQueue(100));
 
 var host = builder.Build();
+
+var controller = host.Services.GetRequiredService<Controller>();
+controller.Start();
+
 host.Run();
