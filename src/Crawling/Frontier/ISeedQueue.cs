b654ec57da6380ce926ddd7ff65aa821d180d9e5
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-06-28 23:24:20 +0100

    type to handle queueing

---
+++ src/Crawling/Frontier/ISeedQueue.cs
@@ -0,0 +1,10 @@
+﻿namespace Crawling.Frontier;
+
+public interface ISeedQueue<T>
+{
+    Task EnqueueAsync(T item, CancellationToken cancellationToken);
+
+    Task<T> DequeueAsync(CancellationToken cancellationToken);
+
+    int GetCount();
+}
---
+++ src/Crawling/Frontier/SeedQueue.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+using Crawling.CrawlSource;
+
+namespace Crawling.Frontier;
+
+public class SeedQueue : ISeedQueue<ICrawlSource>
+{
+    private readonly Channel<ICrawlSource> _channel;
+
+    private SeedQueue()
+    {
+        var options = new BoundedChannelOptions(100);
+        _channel = Channel.CreateBounded<ICrawlSource>(options);
+    }
+
+    public async Task EnqueueAsync(ICrawlSource item, CancellationToken cancellationToken)
+    {
+        await _channel.Writer.WriteAsync(item, cancellationToken);
+    }
+
+    public async Task<ICrawlSource> DequeueAsync(CancellationToken cancellationToken)
+    {
+        return await _channel.Reader.ReadAsync(cancellationToken);
+    }
+
+    public int GetCount() => _channel.Reader.Count;
+}
