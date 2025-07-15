03d6d16916fc21ce69397eadd4df22ec4c437226
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-07-15 22:31:46 +0100

    pull out crawling logic to its own method

--- src/Crawling/WebCrawler.cs
+++ src/Crawling/WebCrawler.cs
@@ -1,4 +1,6 @@
-﻿using Crawling.Frontier;
+﻿using System.Collections.Concurrent;
+using Crawling.CrawlSource;
+using Crawling.Frontier;
 using Crawling.Harvesting;
 using Crawling.LinkVisiting;
 using MediatR;
@@ -12,41 +14,47 @@ public class WebCrawler(IMediator mediator, ILinkVisitor linkVisitor)
     {
         await mediator.Publish(new UriDiscoveredNotification(configuration.Seed, configuration.Depth), cancellationToken);
         var currentWidth = 0;
-            
+
         // TODO: Make this handle requests in parallel
         // Matches the return of medaitor.Send(seedRequest) to { } and if so assign to source;
         // { } is a property or object pattern, it matches any object that has accessible properties
-        while (await mediator.Send(new SeedRequest(), cancellationToken) is { } queuedSource && currentWidth++ < configuration.Width)
+        while (await mediator.Send(new SeedRequest(), cancellationToken) is { } queuedSource &&
+               currentWidth++ < configuration.Width)
         {
             var source = await queuedSource;
-            
-            while (source.Queue.TryDequeue(out var current, out var currentDepth) && currentDepth < source.Depth)
-            {
-                var client = source.CreateClient();
-                
-                var html = await linkVisitor.VisitAsync(client, current, cancellationToken);
-                if (!string.IsNullOrEmpty(html))
-                {
-                    foreach (var link in LinkHarvester.Harvest(html))
-                    {
-                        if (!source.CanVisit(link))
-                        {
-                            continue;
-                        }
+            await Crawl(source, configuration.Depth, cancellationToken);
+        }
+    }
 
-                        if (source.Seen.TryInsert(link))
-                        {
-                            source.Queue.Enqueue(link, currentDepth + 1);
-                        }
-                        else
-                        {
-                            _ = mediator.Publish(new UriDiscoveredNotification(link, configuration.Depth), cancellationToken);
-                        }
+    public async Task Crawl(ICrawlSource source, int depth, CancellationToken cancellationToken)
+    {
+        while (source.Queue.TryDequeue(out var current, out var currentDepth) && currentDepth < source.Depth)
+        {
+            var client = source.CreateClient();
+
+            var html = await linkVisitor.VisitAsync(client, current, cancellationToken);
+            if (!string.IsNullOrEmpty(html))
+            {
+                foreach (var link in LinkHarvester.Harvest(html))
+                {
+                    if (!source.CanVisit(link))
+                    {
+                        continue;
+                    }
+
+                    if (source.Seen.TryInsert(link))
+                    {
+                        source.Queue.Enqueue(link, currentDepth + 1);
+                    }
+                    else
+                    {
+                        _ = mediator.Publish(new UriDiscoveredNotification(link, depth),
+                            cancellationToken);
                     }
                 }
-
-                await Task.Delay(source.Robot.DelayMs, cancellationToken);
             }
+
+            await Task.Delay(source.Robot.DelayMs, cancellationToken);
         }
     }
 }
