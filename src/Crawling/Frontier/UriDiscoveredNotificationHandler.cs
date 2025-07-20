5ff3024850ceccb9976d5f9596b94973322a0a2c
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-07-20 21:32:36 +0100

    use host for now

--- src/Crawling/Frontier/UriDiscoveredNotificationHandler.cs
+++ src/Crawling/Frontier/UriDiscoveredNotificationHandler.cs
@@ -4,22 +4,26 @@ using MediatR;
 
 namespace Crawling.Frontier;
 
-internal sealed class UriDiscoveredNotificationHandler(IVisitationPolicy policy, ICrawlSourceFactory factory, ISeedQueue<Task<ICrawlSource>> seedQueue, Configuration configuration) : INotificationHandler<UriDiscoveredNotification>
+internal sealed class UriDiscoveredNotificationHandler(
+    IVisitationPolicy policy,
+    ICrawlSourceFactory factory,
+    ISeedQueue<Task<ICrawlSource>> seedQueue,
+    Configuration configuration) : INotificationHandler<UriDiscoveredNotification>
 {
     // TODO - Wrap this in its own type
     // TODO - uri normalization and figure out how to ensure we don't requeue the same base uris
-    private static ConcurrentDictionary<Uri, byte> _seen = [];
+    private static readonly ConcurrentDictionary<string, byte> Seen = [];
     private static int _currentWidth;
-    
+
     public async Task Handle(UriDiscoveredNotification notification, CancellationToken cancellationToken)
     {
-        if (_seen.TryAdd(notification.Seed, 1) 
-            && await policy.ShouldVisit(notification.Seed, cancellationToken) 
-            && Interlocked.Increment(ref _currentWidth) <= configuration.Width  )
+        if (Seen.TryAdd(notification.Seed.Host, 1) 
+            && await policy.ShouldVisit(notification.Seed, cancellationToken)
+            && Interlocked.Increment(ref _currentWidth) <= configuration.Width)
         {
-            var createCrawlSourceTask = factory.Create(notification.Seed.ToString(), notification.Depth, cancellationToken);
+            var createCrawlSourceTask =
+                factory.Create(notification.Seed.ToString(), notification.Depth, cancellationToken);
             await seedQueue.EnqueueAsync(createCrawlSourceTask, cancellationToken);
-
         }
     }
 }
