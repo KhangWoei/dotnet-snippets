8a244663fc1d910337d87bc8d5697797a0e2ba90
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-07-28 22:34:16 +0100

    introduce commands

--- sandbox/NpgsqlTesting/Trie/TrieTreeCommands.cs
+++ sandbox/NpgsqlTesting/Trie/TrieTreeCommands.cs
@@ -1,6 +1,24 @@
-﻿namespace NpgsqlTesting.Trie;
+﻿using System.Data;
+using Npgsql;
+
+namespace NpgsqlTesting.Trie;
 
 public class TrieTreeCommands
 {
-    
+    public async Task Create(TrieTreeModel tree)
+    {
+        await using var connection = new NpgsqlConnection(Connection.ConnectionString);
+        await connection.OpenAsync();
+
+        var command = connection.CreateCommand();
+        command.CommandText = """
+                              INSERT INTO trees (name, base_url)
+                              VALUES (@n, @u);
+                              """;
+        
+        command.Parameters.AddWithValue("n", tree.Name);
+        command.Parameters.AddWithValue("u", tree.BaseUrl);
+
+        await command.ExecuteNonQueryAsync();
+    }
 }
