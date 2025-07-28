ab57d2124f360dfe10fea1d56373d6a04947e096
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-07-28 22:34:29 +0100

    add some tests

---
+++ sandbox/NpgsqlTesting/TrieTests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NpgsqlTesting.Trie;
+using NUnit.Framework;
+
+namespace NpgsqlTesting.TrieTests;
+
+[TestFixture]
+public class Tests
+{
+    [TestCase]
+    public async Task CreateAndGet()
+    {
+        var command = new TrieTreeCommands();
+        var queries = new TrieTreeQueries();
+
+        var expected = new TrieTreeModel("test", "some_url");
+        await command.Create(expected);
+
+        var actual = await queries.Get(expected.Name);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Name, Is.EqualTo(expected.Name));
+            Assert.That(actual.BaseUrl, Is.EqualTo(expected.BaseUrl));
+        });
+    }
+}
