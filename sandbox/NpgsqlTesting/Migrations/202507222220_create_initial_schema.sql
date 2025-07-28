1f7d47c496a86fea371911d579ab0b73a595f753
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-07-28 23:22:02 +0100

    helper to spin up databases for tests

---
+++ sandbox/NpgsqlTesting/Migrations/202507222220_create_initial_schema.sql
@@ -0,0 +1,15 @@
+create table trees (
+    id SERIAL PRIMARY KEY, 
+    name varchar(255) not null, 
+    base_url text, 
+    created_at timestamptz default now(), 
+    constraint unique_trees_name UNIQUE(name)
+);
+
+create table nodes (
+    id bigserial primary key, 
+    tree_id integer not null references trees(id) on delete cascade, 
+    parent_id bigint references nodes(id) on delete cascade, 
+    is_terminal boolean default false, 
+    created_at timestamptz default now()
+);
---
+++ sandbox/NpgsqlTesting/TrieTests/TestDatabaseHelper.cs
@@ -0,0 +1,72 @@
+﻿using Npgsql;
+
+namespace NpgsqlTesting.TrieTests;
+
+public class TestDatabaseHelper(string connectionString, string databaseName = "") 
+{
+    private readonly string _databaseName = string.IsNullOrEmpty(databaseName) ? $"test_{Guid.NewGuid()}" : databaseName;
+    
+    public async Task<string> CreateDatabaseAsync()
+    {
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+        
+        var command = connection.CreateCommand();
+        command.CommandText = $"""
+                              CREATE DATABASE "{_databaseName}"
+                              """;
+
+        await command.ExecuteNonQueryAsync();
+        
+        var databaseConnection = new NpgsqlConnectionStringBuilder(connectionString)
+        {
+            Database = _databaseName
+        }.ToString();
+        
+        await RunMigrations(databaseConnection);
+
+        return databaseConnection;
+    }
+
+    private async Task RunMigrations(string databaseConnection)
+    {
+        var assembly = typeof(TestDatabaseHelper).Assembly;
+        var resourceNames = assembly.GetManifestResourceNames();
+        var migrations = resourceNames.Where(n => n.EndsWith(".sql")).Order();
+
+        await using var connection = new NpgsqlConnection(databaseConnection);
+        await connection.OpenAsync();
+        
+        foreach (var migration in migrations)
+        {
+            await using var stream = assembly.GetManifestResourceStream(migration);
+            using var reader = new StreamReader(stream!);
+
+            var sql = await reader.ReadToEndAsync();
+            var command = connection.CreateCommand();
+
+            command.CommandText = sql;
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+
+    public async ValueTask Cleanup()
+    {
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = $"""
+                               SELECT pg_terminate_backend(pid)
+                               FROM pg_stat_activity
+                               WHERE datname = '{_databaseName}' AND pid <> pg_backend_pid()
+                               """;
+        await command.ExecuteNonQueryAsync();
+
+        command.CommandText = $"""
+                               DROP DATABASE IF EXISTS "{_databaseName}";
+                               """;
+        await command.ExecuteNonQueryAsync();
+    }
+}
