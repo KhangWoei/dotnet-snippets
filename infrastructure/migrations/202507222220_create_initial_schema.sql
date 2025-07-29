ddaafe00817e0bb8e519be28f50188d94eb4def8
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-07-29 23:18:37 +0100

    ammend migration script

--- infrastructure/migrations/202507222220_create_initial_schema.sql
+++ infrastructure/migrations/202507222220_create_initial_schema.sql
@@ -10,6 +10,7 @@ create table nodes (
     id bigserial primary key, 
     tree_id integer not null references trees(id) on delete cascade, 
     parent_id bigint references nodes(id) on delete cascade, 
+    full_path text not null,
     is_terminal boolean default false, 
     created_at timestamptz default now()
 );
