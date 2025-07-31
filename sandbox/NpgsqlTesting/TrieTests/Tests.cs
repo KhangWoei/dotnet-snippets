cf92cc0e5b0671bc7511c9f75ad54fb704198920
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-07-31 22:28:12 +0100

    simple test

--- sandbox/NpgsqlTesting/TrieTests/Tests.cs
+++ sandbox/NpgsqlTesting/TrieTests/Tests.cs
@@ -37,7 +37,7 @@ public class Tests
     public class NodeTests
     {
         [TestCase]
-        public async Task CreateAndGet()
+        public async Task CreateAndGet_NoParent()
         {
 
             var testDatabaseHelper =
@@ -63,6 +63,38 @@ public class Tests
             {
                 Assert.That(actual.Path, Is.EqualTo(createNodeRequest.Path));
                 Assert.That(actual.IsTerminal, Is.EqualTo(createNodeRequest.IsTerminal));
+            });
+        }
+        
+        [TestCase]
+        public async Task CreateAndGet_WithParent()
+        {
+
+            var testDatabaseHelper =
+                new TestDatabaseHelper("Host=localhost,5432;Username=postgres;Password=Password@1;");
+            var databaseConnection = await testDatabaseHelper.CreateDatabaseAsync();
+
+            var treeCommands = new TrieTreeCommands(databaseConnection);
+            var treeQueries = new TrieTreeQueries(databaseConnection);
+            var nodeCommands = new NodeCommands(databaseConnection);
+            var nodeQueries = new NodeQueries(databaseConnection);
+
+            var createTreeRequest = new CreateTreeRequest("test", "some_url");
+            await treeCommands.CreateAsync(createTreeRequest);
+            
+            var tree = await treeQueries.GetAsync(createTreeRequest.Name);
+            var createRootNodeRequest = new CreateNodeRequest(tree.Id, null ,"/", false);
+            await nodeCommands.CreateAsync(createRootNodeRequest);
+
+            var root = await nodeQueries.GetAsync(tree.Id, createRootNodeRequest.Path);
+            var createChildNodeRequest = new CreateNodeRequest(tree.Id, root.Id, "/test", true);
+            await nodeCommands.CreateAsync(createChildNodeRequest);
+            var actual = await nodeQueries.GetAsync(tree.Id, createChildNodeRequest.Path);
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Path, Is.EqualTo(createChildNodeRequest.Path));
+                Assert.That(actual.IsTerminal, Is.EqualTo(createChildNodeRequest.IsTerminal));
             });
         }
     }
