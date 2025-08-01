d9ca643c7bd94d549dad701aa8e088f0217f2e38
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-01 23:07:11 +0100

    create tree and nodes objects

---
+++ sandbox/NpgsqlTesting/Trie/Node.cs
@@ -0,0 +1,6 @@
+﻿namespace NpgsqlTesting.Trie;
+
+public record Node(Node? Parent, Dictionary<string, Node> Children, bool IsTerminal, string Path)
+{
+    public bool IsTerminal { get; set; } = IsTerminal;
+}
---
+++ sandbox/NpgsqlTesting/Trie/Tree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace NpgsqlTesting.Trie;
+
+public record Tree(string Name, string BaseUrl) : IEnumerable<(Node Node, string Path)>
+{
+    private readonly Node _root = new(null, [], true, "");
+
+    public bool TryInsert(string value)
+    {
+        var current = _root;
+        var parts = value.Split('/', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var part in parts)
+        {
+            if (!current.Children.TryGetValue(part, out var child))
+            {
+                child = new Node(current, [], false, part);
+                current.Children[part] = child;
+            }
+
+            current = child;
+        }
+
+        current.IsTerminal = true;
+        return true;
+    }
+    
+    public IEnumerator<(Node Node, string Path)> GetEnumerator()
+    {
+        return new TreeIterator(_root);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
