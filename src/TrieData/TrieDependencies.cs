344bd41ed938452d4a4aa6e525e333f7c68df48d
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-05 22:16:30 +0100

    setup npgsql data source

---
+++ src/TrieData/TrieDependencies.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace TrieData;
+
+public static class TrieDataDependencySetup
+{
+    public static void UserTrieData(this IServiceCollection services, NpgsqlConnectionStringBuilder connectionStringBuilder)
+    {
+        services.AddNpgsqlDataSource(connectionStringBuilder.ToString());
+    }
+}
