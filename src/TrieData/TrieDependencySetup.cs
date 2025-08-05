4c6ebb62462615c42b7d2c441bfd41d9ca421f4f
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-05 23:26:40 +0100

    register services

--- src/TrieData/TrieDependencySetup.cs
+++ src/TrieData/TrieDependencySetup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
+using TrieData.Nodes;
+using TrieData.Tries;
 
 namespace TrieData;
 
@@ -8,6 +10,13 @@ public static class TrieDataDependencySetup
     public static void UserTrieData(this IServiceCollection services, NpgsqlConnectionStringBuilder connectionStringBuilder)
     {
         services.AddNpgsqlDataSource(connectionStringBuilder.ToString());
+
+        services.AddScoped<ITriesReadRepository, TriesReadRepository>();
+        services.AddScoped<ITriesWriteRepository, TriesWriteRepository>();
+
+        services.AddScoped<INodesReadRepository, NodesReadRepository>();
+        services.AddScoped<INodesWriteRepository, NodesWriteRepository>();
+        
         services.AddMediatR(c => c.RegisterServicesFromAssembly(typeof(TrieDataDependencySetup).Assembly));
     }
 }
