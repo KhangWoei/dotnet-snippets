a7121af38a4cf6dc8deb5f459c4c0dc0b7a68ca5
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-09 22:02:38 +0100

    baisc logs

--- src/Crawling/WebCrawler.cs
+++ src/Crawling/WebCrawler.cs
@@ -3,11 +3,13 @@ using Crawling.CrawlSource;
 using Crawling.Frontier;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TrieData;
+using ILogger = Serilog.ILogger;
 
 namespace Crawling;
 
-public sealed class WebCrawler(IServiceProvider serviceProvider, IMediator mediator, Configuration configuration, ICrawler crawler) : IDisposable
+public sealed class WebCrawler(IServiceProvider serviceProvider, IMediator mediator, Configuration configuration, ICrawler crawler, ILogger logger) : IDisposable
 {
     private sealed record CrawlTask(Task Task, CancellationTokenSource CancellationTokenSource);
     
@@ -16,7 +18,9 @@ public sealed class WebCrawler(IServiceProvider serviceProvider, IMediator media
         
     public async Task Start(CancellationToken cancellationToken)
     {
+        var loggerWithContext = logger.ForContext<WebCrawler>();
         
+        loggerWithContext.Information("Queuing initial seeds");
         await mediator.Publish(new UriDiscoveredNotification(configuration.Seed, configuration.Depth), cancellationToken);
 
         while (!cancellationToken.IsCancellationRequested)
@@ -30,7 +34,8 @@ public sealed class WebCrawler(IServiceProvider serviceProvider, IMediator media
                     await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken);
                     continue;
                 }
-
+                
+                loggerWithContext.Information("Crawl complete");
                 break;
             }
 
@@ -40,7 +45,8 @@ public sealed class WebCrawler(IServiceProvider serviceProvider, IMediator media
 
                 var taskId = Guid.NewGuid();
                 var cancellationTokenSource = new CancellationTokenSource();
-                var task = ProcessCrawlTask(queuedTask, () => _semaphore.Release(), cancellationTokenSource.Token);
+                
+                var task = ProcessCrawlTask(queuedTask, () => _semaphore.Release(), loggerWithContext, cancellationTokenSource.Token);
                 var crawlTask = new CrawlTask(task, cancellationTokenSource);
 
                 _tasks.TryAdd(taskId, crawlTask);
@@ -55,33 +61,37 @@ public sealed class WebCrawler(IServiceProvider serviceProvider, IMediator media
             }
             catch (OperationCanceledException)
             {
+                loggerWithContext.Debug("Operation canceled");
             }
         }
     }
 
-    private async Task ProcessCrawlTask(Task<ICrawlSource> source, Action onComplete, CancellationToken cancellationToken)
+    private async Task ProcessCrawlTask(Task<ICrawlSource> source, Action onComplete, ILogger loggerWithContext, CancellationToken cancellationToken)
     {
         try
         {
             var trie = await crawler.Crawl(await source, configuration.Depth, cancellationToken);
             
-            await WriteToFile(trie, configuration.Output, cancellationToken);
+            await WriteToFile(trie, configuration.Output, loggerWithContext, cancellationToken);
 
             // TODO: Fix this - i need some logging
             if (serviceProvider.GetService(typeof(IRequestHandler<>).MakeGenericType(typeof(CreateTrieCommandRequest))) is not null)
             {
+                loggerWithContext.Information("Sending {request}", nameof(CreateTrieCommandRequest));
                 await mediator.Send(new CreateTrieCommandRequest(trie), cancellationToken);
             }
         }
         finally
         {
+            loggerWithContext.Information("");
             onComplete.Invoke();
         }
     }
 
     // TODO: Could be improved by using a StreamWriter
-    private static async Task WriteToFile(Trie trie, DirectoryInfo directory, CancellationToken cancellationToken)
+    private static async Task WriteToFile(Trie trie, DirectoryInfo directory, ILogger logger, CancellationToken cancellationToken)
     {
+        logger.Information("Creating dot file for {name}", trie.Name);
         var dotNotation = trie.ToDotNotation();
         var filePath = Path.Combine(directory.FullName, $"{trie.Name}.dot");
         await File.WriteAllTextAsync(filePath, dotNotation, cancellationToken);
