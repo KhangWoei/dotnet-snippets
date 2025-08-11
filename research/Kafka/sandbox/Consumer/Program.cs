aa220a733af8cffd17ad380ceebab5e3978e8c3a
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-11 22:38:03 +0100

    implement simple consumer

--- research/Kafka/sandbox/Consumer/Program.cs
+++ research/Kafka/sandbox/Consumer/Program.cs
@@ -1,8 +1,46 @@
-﻿namespace Consumer;
+﻿using Confluent.Kafka;
+
+namespace Consumer;
 
 public static class Consumer
 {
-    public static async Task Main(string[] args)
+    private static readonly CancellationTokenSource CancellationTokenSource = new();
+
+    public static void Main(string[] args)
     {
+        var configuration = new ConsumerConfig
+        {
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            BootstrapServers = "localhost:99092",
+            GroupId = "test-consumer-group"
+        };
+
+        var consumerBuilder = new ConsumerBuilder<Null, string>(configuration);
+
+        using var consumer = consumerBuilder.Build();
+
+        consumer.Subscribe("test-topic");
+
+        Console.CancelKeyPress += (_, e) =>
+        {
+            e.Cancel = true;
+            CancellationTokenSource.Cancel();
+        };
+
+        while (!CancellationTokenSource.IsCancellationRequested)
+        {
+            try
+            {
+                var message = consumer.Consume(CancellationTokenSource.Token); //polling is handled by the consume method, this is blocking
+                Console.WriteLine(
+                    $"Consumed message '{message.Message.Value}' at: '{message.TopicPartitionOffset}'");
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Error occured: {e.Error.Reason}");
+            }
+        }
+        
+        consumer.Close();
     }
 }
