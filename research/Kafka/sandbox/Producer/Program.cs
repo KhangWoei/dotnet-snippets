df82613ef8c85165369daf6a23c95cc2a149b5ea
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-08-11 22:26:26 +0100

    simple command line app kafka producer

--- research/Kafka/sandbox/Producer/Program.cs
+++ research/Kafka/sandbox/Producer/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.Loader;
+using Confluent.Kafka;
+
+namespace Producer;
+
+public static class Program
+{
+    private static readonly CancellationTokenSource CancellationTokenSource = new();
+
+    public static async Task Main(string[] args)
+    {
+        var configuration = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        var producerBuilder = new ProducerBuilder<Null, string>(configuration);
+        using var producer = producerBuilder.Build();
+
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            e.Cancel = true;
+            CancellationTokenSource.CancelAsync();
+        };
+
+        while (!CancellationTokenSource.Token.IsCancellationRequested)
+        {
+            Console.WriteLine("Input:");
+            var input = Console.ReadLine() ?? string.Empty;
+
+            try
+            {
+                var message = await producer.ProduceAsync("test-topic", new Message<Null, string>() { Value = input });
+                Console.WriteLine($"Delivered '{message.Value}' to {message.TopicPartitionOffset}");
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+
+        Console.WriteLine("Shutting down...");
+        producer.Flush();
+    }
+}
