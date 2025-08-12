b72c4e7ce8ee602b4df4d79d0e2fae096989aab2
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-08-12 22:51:40 +0100

    fix  infrasturcture configuration

--- research/Kafka/sandbox/01-setup.sh
+++ research/Kafka/sandbox/01-setup.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
+echo "Creating Kafka environment"
 docker-compose -f "${SCRIPT_DIR}/kafka-setup.yaml" up -d 
 
+echo "Creating test-topic"
 kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092
