24ca7aa9a57cdb2f92603c19d66134baa05c4890
KhangWoei <41323253+KhangWoei@users.noreply.github.com>
2025-09-09 22:48:45 +0100

    remove unused using

--- src/Producer/Program.cs
+++ src/Producer/Program.cs
@@ -1,7 +1,6 @@
 using Confluent.Kafka;
 using Confluent.Kafka.Admin;
 using FinnhubClient;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
