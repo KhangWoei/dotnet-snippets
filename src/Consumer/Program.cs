102e4d878d7440f5319d4149176f4e06281da983
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-09-11 23:16:55 +0100

    simple consumer to consume and echo result

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,54 @@
+using Confluent.Kafka;
+using Consumer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+var applicationBuilder = Host.CreateApplicationBuilder();
+applicationBuilder.Services.Configure<ConsumerOptions>(applicationBuilder.Configuration.GetSection(nameof(ConsumerOptions)));
+
+using var host = applicationBuilder.Build();
+
+var options = host.Services.GetRequiredService<IOptions<ConsumerOptions>>().Value;
+
+var configuration = new ConsumerConfig
+{
+    BootstrapServers = options.BootstrapServers,
+    GroupId = "database-write-consumer",
+    EnableAutoOffsetStore = false,
+    EnableAutoCommit = true,
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    EnablePartitionEof = false,
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(configuration).Build();
+
+consumer.Subscribe("test-topic");
+
+var cancellationTokenSource = new CancellationTokenSource();
+
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    cancellationTokenSource.Cancel();
+};
+
+try
+{
+    while (true)
+    {
+        try
+        {
+            var result = consumer.Consume(cancellationTokenSource.Token);
+            Console.WriteLine(result.Message.Value);
+        }
+        catch (ConsumeException exception)
+        {
+            Console.WriteLine(exception);
+        }
+    }
+}
+finally
+{
+    consumer.Close();
+}
