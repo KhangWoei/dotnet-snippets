7c54a3d29c006badb256c6b2fc999845933b0b28
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-09-17 22:26:57 +0100

    remove user from setup scripts

--- infrastructure/database/migrations/00-setup-database.sql
+++ infrastructure/database/migrations/00-setup-database.sql
@@ -3,11 +3,3 @@ CREATE DATABASE market_data;
 CREATE ROLE market_data LOGIN WITH PASSWORD <SOME_PASSWORD>;
 ALTER DATABASE market_data OWNER TO market_data;
 
-CREATE ROLE market_data_reader;
-CREATE ROLE grafana;
-GRANT market_data_reader TO grafana;
-
-CREATE ROLE market_data_writer;
-CREATE ROLE csharp;
-GRANT market_data_writer TO csharp;
-
